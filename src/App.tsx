620d5a40ba1ff1d08ef5dd8fdf74ae2203f6378e
alexandremagno <alz@cluedin.com>
2023-03-01 20:04:34 -0300

    initial react app with table data mock structure for virtualized lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FunctionComponent } from 'react';
+import { styled } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+import Container from '@mui/material/Container';
+import TableRow from '@mui/material/TableRow';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import { VirtualizedList } from './components/VirtualizedList/VirtualizedList';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css';
 
-function App() {
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+type row = {
+  name: string;
+  calories: number;
+  carbs: number;
+  fat: number;
+  protein: number;
+}
+
+type RowProps = {
+  row: row;
+  index: number;
+}
+
+const Row = ({ row , index}:RowProps):JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledTableRow key={`table-row-${index}`}>
+      <StyledTableCell component="th" scope="row">
+        {row.name}
+      </StyledTableCell>
+      <StyledTableCell align="right">{row.calories}</StyledTableCell>
+      <StyledTableCell align="right">{row.fat}</StyledTableCell>
+      <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+      <StyledTableCell align="right">{row.protein}</StyledTableCell>
+    </StyledTableRow>
+  );
+};
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const columnData: Array<string> = [
+  'Dessert (100g serving)',
+  'Calories',
+  'Fat (g)',
+  'Carbs (g)',
+  'Protein (g)',
+]
+
+function App(): JSX.Element {
+  return (
+    <Container maxWidth="lg">
+      <div className="App">
+        <header className="App-header">
+          <Typography variant="h4" gutterBottom>
+            Virtualized lists
+          </Typography>
+        </header>
+        <VirtualizedList
+          renderItem={Row}
+          data={rows}
+          columnData={columnData}
+        />
+      </div>
+    </Container>
   );
 }
 
